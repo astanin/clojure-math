6331c68892bddbe9c32e90a4c1a1a3d20148b537
Sergey Astanin <s.astanin@gmail.com>
2013-03-27 00:35:40 +0100

    conv test for vectors

--- test/clojure/math/signal_test.clj
+++ test/clojure/math/signal_test.clj
@@ -7,10 +7,11 @@
   (testing "conv output is the same like in NumPy:"
     (let [input-array (double-array [1 2 3 4 5])
           input-seq   (range 1 6)
+          input-vec   (vec input-seq)
           kern3 [0.25 0.5 0.25]
           kern4 [0.25 0.5 0.5 0.25]
           kern-long [100000 10000 1000 100 10 1]]
-      (doseq [input [input-array input-seq]]
+      (doseq [input [input-array input-seq input-vec]]
         (let [class-name (str (class input))]
           (testing (str class-name ", :full output, odd-size kernel")
            (is (= [0.25, 1.0, 2.0, 3.0, 4.0, 3.5, 1.25]
