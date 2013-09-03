afd9e1291e518b086d6ff8895ddea543836b91b1
Sergey Astanin <s.astanin@gmail.com>
2013-09-03 12:45:42 +0200

    bump dependencies + EJML

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://github.com/astanin/clojure-math"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.apache.commons/commons-math3 "3.1.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.commons/commons-math3 "3.2"]
+                 [com.googlecode.efficient-java-matrix-library/ejml "0.23"]]
   :profiles {:dev {:dependencies [[criterium "0.3.1"]]
                    :plugins [[codox "0.6.4"] ; lein with-profile dev doc
                              ]}})
